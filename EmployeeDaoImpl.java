d4a4c3fb7c3356e5fcb550aa5e80fc5fc8f5b92c
Santhiya30 <73530516+Santhiya30@users.noreply.github.com>
2023-04-13 12:18:04 +0530

    Add files via upload
    update and delete Activity completed - 12th April activity.

---
+++ EmployeeDaoImpl.java
@@ -0,0 +1,98 @@
+
+package com.dal.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.dal.helper.MyDBConnection;
+import com.dal.model.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao{
+
+	Connection con;
+	PreparedStatement ps;
+	Statement stmt;
+	ResultSet rs;
+	
+	
+	@Override
+	public void insertEmployee(Employee emp) {
+	     
+		try {
+			con  = MyDBConnection.getDbConnection();
+			ps=con.prepareStatement("insert into DB values(?,?)");
+			
+			ps.setInt(1, emp.getEid());
+			ps.setString(2, emp.getEname());
+			int noofrows = ps.executeUpdate();
+			System.out.println(noofrows  + " got inserted... ");
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public void showEmployee() {	
+		try {
+			con  = MyDBConnection.getDbConnection();
+			stmt= con.createStatement();
+			rs = stmt.executeQuery("select * from DB");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " -- " + rs.getString(2));
+				
+			}
+			
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public void updateEmployee(Employee emp) {
+		try {
+			con  = MyDBConnection.getDbConnection();
+			ps=con.prepareStatement("insert into DB values(?,?)");
+			
+			ps.setInt(1, emp.getEid());
+			ps.setString(2, emp.getEname());
+			ps.setInt(3, emp.getEid());
+			ps.setString(4, emp.getEname());
+			int rowsUpdated = ps.executeUpdate();
+			if(rowsUpdated > 0) {
+			System.out.println(rowsUpdated  +"An existing user was updated successfully!");
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void deleteEmployee(Employee emp) {
+		try {
+			con  = MyDBConnection.getDbConnection();
+			ps=con.prepareStatement("insert into DB values(?,?)");
+			ps.setString(2, emp.getEname());			
+			int rowsDeleted = ps.executeUpdate();
+			if(rowsDeleted>0) {
+			System.out.println(rowsDeleted  + "A user was deleted successfully!");
+			}
+			} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+
+}
