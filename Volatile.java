e3894285d702da59c5b423887f0125100348f4ee
Santhiya30 <73530516+Santhiya30@users.noreply.github.com>
2023-04-20 12:48:50 +0530

    Add files via upload

--- Volatile.java
+++ Volatile.java
@@ -1,17 +1,48 @@
-class Test extends Thread{
-	int noOfThreads = 5;  
-	public void run()  
-	{  
-		for(int i = 0; i < noOfThreads; ++i)  
-			System.out.println("i = " + noOfThreads);
-		System.out.println("Thread Completed");
-	} 
-     
-}
-public class Volatile{
-	public static void main(String[] args) {
-		Test t = new Test();
-		t.start();
-		System.out.println("Thread Started");
+// Java Program to demonstrate the
+// use of Volatile Keyword in Java
+
+public class Volatile {
+	private static final Logger LOGGER = MyLoggerFactory.getSimplestLogger();
+	private static volatile int MY_INT = 0;
+
+	public static void main(String[] args)
+	{
+		new ChangeListener().start();
+		new ChangeMaker().start();
+	}
+
+	static class ChangeListener extends Thread {
+		@Override public void run()
+		{
+			int local_value = MY_INT;
+			while (local_value < 5) {
+				if (local_value != MY_INT) {
+					LOGGER.log(
+						Level.INFO,
+						"Got Change for MY_INT : {0}",
+						MY_INT);
+					local_value = MY_INT;
+				}
+			}
+		}
+	}
+
+	static class ChangeMaker extends Thread {
+		@Override public void run()
+		{
+			int local_value = MY_INT;
+			while (MY_INT < 5) {
+				LOGGER.log(Level.INFO,
+						"Incrementing MY_INT to {0}",
+						local_value + 1);
+				MY_INT = ++local_value;
+				try {
+					Thread.sleep(500);
+				}
+				catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
