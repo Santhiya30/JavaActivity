0674e731cfd6adb09f462657d0ac388e608ffa44
Santhiya30 <73530516+Santhiya30@users.noreply.github.com>
2023-04-24 10:56:45 +0530

    21st April Activity -  Abstract design pattern

---
+++ abfacdp/CheesePizza.java
@@ -0,0 +1,42 @@
+package abfacdp;
+
+public class CheesePizza extends AbstractFactory {
+    public CheesePizza() {
+        name = "Cheese Pizza";
+        dough = "Regular Crust";
+        sauce = "Marinara Pizza Sauce";
+        toppings.add("Fresh Mozzarella");
+        toppings.add("Parmesan");
+    }
+}
+
+ class ClamPizza extends AbstractFactory {
+    public ClamPizza() {
+        name = "Clam Pizza";
+        dough = "Thin crust";
+        sauce = "White garlic sauce";
+        toppings.add("Clams");
+        toppings.add("Grated parmesan cheese");
+    }
+}
+
+ class VeggiePizza extends AbstractFactory {
+    public VeggiePizza() {
+        name = "Veggie Pizza";
+        dough = "Crust";
+        sauce = "Marinara sauce";
+        toppings.add("Diced onion");
+        toppings.add("Sliced mushrooms");
+        toppings.add("pepper");
+    }
+}
+ class PepperoniPizza extends AbstractFactory {
+    public PepperoniPizza() {
+        name = "Pepperoni Pizza";
+        dough = "Crust";
+        sauce = "Marinara sauce";
+        toppings.add("Sliced Onion");
+        toppings.add("cheese");
+    }
+}
+
